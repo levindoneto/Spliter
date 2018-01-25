29f9d1974f9f4f005954b864eea52fd17112cf7b
Levindo Neto <levindogtn@gmail.com>
2018-01-25 17:39:41 +0100

    Add changing directory

--- Directory.py
+++ Directory.py
@@ -13,3 +13,11 @@ def createSubDir(fileName):
     subdirectoryName = Utils.getSubDirName(fileNameWithoutExtension)
     if not (os.path.exists(subdirectoryName)):
         os.makedirs(subdirectoryName)
+
+'''
+Function that changes the current directory to a one passed as the parameter
+@Parameter: String: Directory's name
+@Return: Void: It changes the directory
+'''
+def cd(directoryPath):
+    os.chdir(directoryPath)
